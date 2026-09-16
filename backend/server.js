710c83d32d608f1a732c9146eb7628843b493adf
Gr1sm4 <23btrcn082@jainuniversity.ac.in>
2026-09-16 18:56:59 +0530

    implement basic HTTP server for weather backend

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,9 @@
-console.log("Weather backend started!");
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.end("Hello from Weather Backend!");
+});
+
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
